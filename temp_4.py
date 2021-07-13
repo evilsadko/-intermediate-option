e9e6faededec4684931c2c47eed2ed0d86cee8c7
evilsadko <nerevaren@mail.ru>
2021-07-13 21:17:34 +0300

    update

--- temp_4.py
+++ temp_4.py
@@ -10,62 +10,76 @@ import keras
 import tensorflow as tf
 slim = tf.contrib.slim
 
-#def weight_variable(shape):
-#  initial = tf.truncated_normal(shape, stddev=0.1)
-#  return tf.Variable(initial)
+learn_rate=0.001
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
 
 
-#def bias_variable(shape):
-#  initial = tf.constant(0.1, shape=shape)
-#  return tf.Variable(initial)
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
 
-#def get_loss(y, y_):
-#    # Calculate the loss from digits being incorrect.  Don't count loss from
-#    # digits that are in non-present plates.
-#    digits_loss = tf.nn.softmax_cross_entropy_with_logits(
-#                                          logits=tf.reshape(y[:, 2:],
-#                                                     [-1, len(common.CHARS)]),
-#                                          labels=tf.reshape(y_[:, 2:],
-#                                                     [-1, len(common.CHARS)]))
-#    digits_loss = tf.reshape(digits_loss, [-1, 9])
-#    digits_loss = tf.reduce_sum(digits_loss, 1)
-#    digits_loss *= (y_[:, 0] != 0)
-#    digits_loss = tf.reduce_sum(digits_loss)
+def get_loss(y, y_):
+    # Calculate the loss from digits being incorrect.  Don't count loss from
+    # digits that are in non-present plates.
+    digits_loss = tf.nn.softmax_cross_entropy_with_logits(
+                                          logits=tf.reshape(y[:, 1:],
+                                                     [-1, 100]),
+                                          labels=tf.reshape(y_[:, 1:],
+                                                     [-1, 100]))
+    digits_loss = tf.reshape(digits_loss, [-1, 1])
+    digits_loss = tf.reduce_sum(digits_loss, 1)
+    digits_loss *= (y_[:, 0] != 0)
+    digits_loss = tf.reduce_sum(digits_loss)
 
-#    # Calculate the loss from presence indicator being wrong.
-#    #presence_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=y[:, :1], labels=y_[:, :1])
-#    presence_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=y[:, :1], labels=y_[:, :1])
-#    presence_loss = 9 * tf.reduce_sum(presence_loss)
+    # Calculate the loss from presence indicator being wrong.
+    #presence_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=y[:, :1], labels=y_[:, :1])
+    presence_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=y[:, :1], labels=y_[:, :1])
+    presence_loss = 1 * tf.reduce_sum(presence_loss)
 
-#    return digits_loss, presence_loss, digits_loss + presence_loss
+    return digits_loss, presence_loss, digits_loss + presence_loss
 
 
-#def net():
-#    strides = 1
-#    x_expanded = tf.placeholder(tf.float32, [None, 6, 3, 1])
-#    x = slim.conv2d(x_expanded, 32, (1,1), stride=strides, padding="SAME", activation_fn=tf.nn.relu) 
-#    x = slim.conv2d(x, 64, (3, 3), stride=strides, padding="SAME", activation_fn=tf.nn.relu) 
-#    x = slim.conv2d(x, 256, (3, 3), stride=strides, padding="VALID", activation_fn=tf.nn.relu)
-#    print (x.shape)
-#    W_fc1 = weight_variable([4 * 1 * 256, 2048])
-#    b_fc1 = bias_variable([2048])
-#    conv_layer_flat = tf.reshape(x, [-1, 4 * 1 * 256])
-#    h_fc1 = tf.nn.relu(tf.matmul(conv_layer_flat, W_fc1) + b_fc1)
+def net():
+    strides = 1
+    x_expanded = tf.placeholder(tf.float32, [None, 6, 3, 1])
+    x = slim.conv2d(x_expanded, 32, (1,1), stride=strides, padding="SAME", activation_fn=tf.nn.relu) 
+    x = slim.conv2d(x, 64, (3, 3), stride=strides, padding="SAME", activation_fn=tf.nn.relu) 
+    x = slim.conv2d(x, 256, (3, 3), stride=strides, padding="VALID", activation_fn=tf.nn.relu)
+    print (x.shape)
+    W_fc1 = weight_variable([4 * 1 * 256, 2048])
+    b_fc1 = bias_variable([2048])
+    conv_layer_flat = tf.reshape(x, [-1, 4 * 1 * 256])
+    h_fc1 = tf.nn.relu(tf.matmul(conv_layer_flat, W_fc1) + b_fc1)
 
-#    W_fc2 = weight_variable([2048, 3])
-#    b_fc2 = bias_variable([3])
+    W_fc2 = weight_variable([2048, 1+1*100])
+    b_fc2 = bias_variable([1+1*100])
 
-#    y = tf.matmul(h_fc1, W_fc2) + b_fc2
-#    print (y.shape)
-#    return x_expanded, y
-#    
-#x, y = net()
-#y_ = tf.placeholder(tf.float32, [None, 3])
+    y = tf.matmul(h_fc1, W_fc2) + b_fc2
+    print (y.shape)
+    return x_expanded, y
+    
+x, y = net()
+y_ = tf.placeholder(tf.float32, [None, 101])
 
-#loss = get_loss(y, y_)
-#train_step = tf.train.AdamOptimizer(learn_rate).minimize(loss)
-#best = tf.argmax(tf.reshape(y[:, :], [-1, 1, len(classes)]), 2)
-#correct = tf.argmax(tf.reshape(y_[:, :], [-1, 1, len(classes)]), 2)
+digits_loss, presence_loss, loss = get_loss(y, y_)
+train_step = tf.train.AdamOptimizer(learn_rate).minimize(loss)
+best = tf.argmax(tf.reshape(y[:, 1:], [-1, 1, 100]), 2)
+correct = tf.argmax(tf.reshape(y_[:, 1:], [-1, 1, 100]), 2)
+
+
+
+
+#init = tf.initialize_all_variables()
+#saver = tf.train.Saver()
+#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.70)
+#with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+#    if initial_weights is not None:
+#        saver.restore(sess, "model_slim/model.ckpt")
+#    else:    
+#        sess.run(init)
+
 
 
 #### Даннаые
@@ -132,14 +146,19 @@ def func_corr(Z, Kx):
     return temp_list, D       
 
 def func_helper(X, k_m, k_m_before):
-    print (X[k_m][-1], X[k_m_before][-1])
-    P = round(((X[k_m][-1]- X[k_m_before][-1])/ X[k_m][-1]*100), 1)
-    up = np.zeros(2)
-    if P<0:
-        up[0] = 1
-    if P>0:
-        up[1] = 1
-    return up, P
+    #print (X[k_m][-1], X[k_m_before][-1])
+    up = np.zeros(1)
+    if X[k_m][-1] != 0 and X[k_m_before][-1] != 0:
+        P = int(((X[k_m][-1]- X[k_m_before][-1])/ X[k_m][-1]*100))#round(((X[k_m][-1]- X[k_m_before][-1])/ X[k_m][-1]*100), 1)
+        if P<= 0:
+            up[0] = 0
+        if P>0:
+            up[0] = 1
+        if (P*-1) > 100:
+            P = 100
+        return up, P
+    else:
+        return up, 0
 
 
 if __name__ == "__main__":
@@ -193,7 +212,8 @@ if __name__ == "__main__":
 # Нужно получить все продукты и все сопуствующие этих продуктов с корреляцией 
 
     LIST_ARR = []
-    for Kx in p_list[:20]:
+    LIST_ARR_pred = []
+    for Kx in p_list[:7]:
         T = DB.client.execute(f"""
                                 SELECT
                                     SUM(Items_Count) as IC,
@@ -203,64 +223,79 @@ if __name__ == "__main__":
                                 WHERE test.Product_ID = {Kx[0]} 
                                 GROUP BY time
                                 """)         
-        Z = np.array(T)[:,:]#[:,:]
+        Z = np.array(T)[:,:]
         
         if (Z.shape[0]==12):
             
             temp_, D = func_corr(Z[:,0], Kx)
-            #ID_corr_product = random.choice(temp_)
-            #print (Kx[0], Z.shape, len(temp_), ID_corr_product, D[ID_corr_product])
             M = {}
             for i in range(Z.shape[0]):
                 IC = Z[i,0]
                 TA = Z[i,1]
                 if TA>IC:
                     price = TA/IC
-                    if price == 0:
-                        print (">>>>>>>>>>>", price)
+#                    if price == 0:
+#                        print (">>>>>>>>>>>", price)
                 else:
                     price = 0.1    
                 M[Z[i,-1]] = [IC, TA, price] #[price, TA, IC]   
-            #print (Kx, T, M)
             temp_list = list(M.keys())
             for A in temp_:
-                #print (A, D[A])
                 corr_product_info = p_list[dict_pid[A]]
-                #print (f"ID PRODUCT {A} >>>>>>>>>>>>>>>>>", dict_pid[A], dict_category1[corr_product_info[1]], dict_category2[corr_product_info[2]])
                 for i in range(1, len(temp_list)):
                     k_m = temp_list[i]
                     k_m_before = temp_list[i-1]
-                    
-                    #P = round(((M[k_m][-1]-M[k_m_before][-1])/M[k_m][0]*100), 1)#int((temp_list[i]-temp_list[i-1])/temp_list[i]*100)
+                    #----------------------------------->
                     up1, P1 = func_helper(M, k_m, k_m_before)
-                    
                     up2, P2 = func_helper(D[A], k_m, k_m_before)
-#                    P = round(((D[A][k_m][-1]- D[A][k_m_before][-1])/ D[A][k_m][0]*100), 1)
-#                    up = np.zeros(2)
-#                    if P<0:
-#                        up[0] = 1
-#                    if P>0:
-#                        up[1] = 1
-                    #print ("...........................")#, k_m, M[k_m_before])
-                    #print (f"ID PRODUCT ОСНОВНОЕ {Kx[0]} >>>>", up1, P1, ">>>>>>>>>>>>>", 
-                    #        dict_pid[Kx[0]], dict_category1[Kx[1]], dict_category2[Kx[2]], M[k_m][-1], M[k_m_before], k_m, k_m_before) 
                     
-                    #print (f"ID PRODUCT КОРРЕЛИРУЮЩЕЕ {A} >>>>", up2, P2, ">>>>>>>>>>>>>", 
-                    #        dict_pid[A], dict_category1[corr_product_info[1]], dict_category2[corr_product_info[2]], D[A][k_m][-1], D[A][k_m_before], k_m, k_m_before) 
-                    #Product_ID
-                    predict = [up1, P1]
+                    print ("...........................")
+                    print (f"ID PRODUCT ОСНОВНОЕ {Kx[0]} >>>>", up1, P1, ">>>>>>>>>>>>>", 
+                            dict_pid[Kx[0]], dict_category1[Kx[1]], dict_category2[Kx[2]], M[k_m][-1], M[k_m_before], k_m, k_m_before) 
+                    
+                    print (f"ID PRODUCT КОРРЕЛИРУЮЩЕЕ {A} >>>>", up2, P2, ">>>>>>>>>>>>>", 
+                            dict_pid[A], dict_category1[corr_product_info[1]], dict_category2[corr_product_info[2]], D[A][k_m][-1], D[A][k_m_before], k_m, k_m_before) 
+                    print ("...........................")
+                    
+                    def char_to_vec(c):
+                        y = numpy.zeros((100,))
+                        #print c
+                        y[c] = 1.0
+                        return y
+                    predict =  numpy.concatenate([up1, char_to_vec(P1)])
+                    #predict = [up1, P1]
                     in_data = [dict_pid[Kx[0]], dict_category1[Kx[1]], dict_category2[Kx[2]], 
                                M[k_m_before][0],  M[k_m_before][1],  M[k_m_before][2],
                                k_m, k_m_before, M[k_m][-1],
                                dict_pid[A], dict_category1[corr_product_info[1]], dict_category2[corr_product_info[2]],
                                D[A][k_m_before][0], D[A][k_m_before][1], D[A][k_m_before][2],
                                k_m, k_m_before, D[A][k_m][-1]]
-                    arr_ = np.array(in_data).reshape((6,3))
+                    arr_ = np.array(in_data).reshape((6,3,1))
                     LIST_ARR.append(arr_)
+                    predict = np.array(predict)
+                    LIST_ARR_pred.append(predict)
+
                     #print (arr_, arr_.shape)
                     #LIST_ARR.append([[up1, P1],[dict_pid[Kx[0]], dict_category1[Kx[1]], dict_category2[Kx[2]], ]])
-                print ("\n")
-    print (len(LIST_ARR))
+                #print ("\n")
+    Array1 = np.array(LIST_ARR)
+    Array2 = np.array(LIST_ARR_pred)
+    print (Array1.shape, Array2.shape)
+    init = tf.initialize_all_variables()
+    saver = tf.train.Saver()
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.70)
+    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+        sess.run(init)
+        for q in range(Array1.shape[0]):
+            batch_xs = Array1[q,:,:,:].reshape((1,6,3,1))
+            batch_ys = Array2[q,:].reshape((1,101))
+            #print (batch_xs.shape, batch_ys.shape)
+            sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+            r = sess.run([best,
+                          correct,
+                          loss],
+                          feed_dict={x: batch_xs, y_: batch_ys})
+            print (r)
 #Какойто из месяцов будет брать в тестовую выборку
 
 

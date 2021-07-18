e312bdd85220bec44ac0971d1a720b49b75d8964
evilsadko <nerevaren@mail.ru>
2021-07-18 11:20:00 +0300

    update

--- temp_6.py
+++ temp_6.py
@@ -24,22 +24,24 @@ def bias_variable(shape):
 def get_loss(y, y_):
     # Calculate the loss from digits being incorrect.  Don't count loss from
     # digits that are in non-present plates.
-    digits_loss = tf.nn.softmax_cross_entropy_with_logits(
-                                          logits=tf.reshape(y[:, 1:],
-                                                     [-1, 100]),
-                                          labels=tf.reshape(y_[:, 1:],
-                                                     [-1, 100]))
-    digits_loss = tf.reshape(digits_loss, [-1, 1])
-    digits_loss = tf.reduce_sum(digits_loss, 1)
-    digits_loss *= (y_[:, 0] != 0)
-    digits_loss = tf.reduce_sum(digits_loss)
+#    digits_loss = tf.nn.softmax_cross_entropy_with_logits(
+#                                          logits=tf.reshape(y[:, 1:],
+#                                                     [-1, 100]),
+#                                          labels=tf.reshape(y_[:, 1:],
+#                                                     [-1, 100]))
+#    digits_loss = tf.reshape(digits_loss, [-1, 1])
+#    digits_loss = tf.reduce_sum(digits_loss, 1)
+#    digits_loss *= (y_[:, 0] != 0)
+#    digits_loss = tf.reduce_sum(digits_loss)
 
-    # Calculate the loss from presence indicator being wrong.
+#    # Calculate the loss from presence indicator being wrong.
+#    presence_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=y[:, :1], labels=y_[:, :1])
+#    presence_loss = 1 * tf.reduce_sum(presence_loss)
+
+#    return digits_loss, presence_loss, digits_loss + presence_loss
     presence_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=y[:, :1], labels=y_[:, :1])
     presence_loss = 1 * tf.reduce_sum(presence_loss)
-
-    return digits_loss, presence_loss, digits_loss + presence_loss
-
+    return presence_loss
 
 def net():
     strides = 1
@@ -63,7 +65,8 @@ def net():
 x, y = net()
 y_ = tf.placeholder(tf.float32, [None, 101])
 
-digits_loss, presence_loss, loss = get_loss(y, y_)
+#digits_loss, presence_loss, loss = get_loss(y, y_)
+loss = get_loss(y, y_)
 train_step = tf.train.AdamOptimizer(learn_rate).minimize(loss)
 best = tf.argmax(tf.reshape(y[:, 1:], [-1, 1, 100]), 2)
 correct = tf.argmax(tf.reshape(y_[:, 1:], [-1, 1, 100]), 2)
@@ -148,6 +151,7 @@ def func_helper(X, k_m, k_m_before):
     #print (X[k_m][-1], X[k_m_before][-1])
     up = np.zeros(1)
     if X[k_m][-1] != 0 and X[k_m_before][-1] != 0:
+        
         P = int(((X[k_m][-1]- X[k_m_before][-1])/ X[k_m][-1]*100))#round(((X[k_m][-1]- X[k_m_before][-1])/ X[k_m][-1]*100), 1)
         if P<= 0:
             up[0] = 0
@@ -159,6 +163,7 @@ def func_helper(X, k_m, k_m_before):
         #print (P, X[k_m][-1], X[k_m_before][-1])
         return up, P
     else:
+        
         return up, 0
 
 def train(initial_weights):    
@@ -248,6 +253,7 @@ def create_data():
     
     LIST_ARR_test = []
     LIST_ARR_pred_test = []
+
     for ikx, Kx in enumerate(p_list[:40]):
         T = DB.client.execute(f"""
                                 SELECT
@@ -276,10 +282,11 @@ def create_data():
                 #print (temp_list, list(M.values()))
                 for A in temp_corr[:3]:
                     corr_product_info = p_list[dict_pid[A]]
-                    for i in range(1, len(temp_list)):
+                    for i in range(0, len(temp_list)):
                         k_m = temp_list[i]
                         k_m_before = temp_list[i-1]
                         #----------------------------------->
+                        
                         up1, P1 = func_helper(M, k_m, k_m_before)
                         up2, P2 = func_helper(D[A], k_m, k_m_before)
                         
@@ -293,7 +300,6 @@ def create_data():
                         
                         def to_vec(c):
                             y = numpy.zeros((100,))
-                            print (c)
                             y[c] = 1.0
                             return y
                         predict =  numpy.concatenate([up1, to_vec(P1)])
@@ -314,16 +320,23 @@ def create_data():
                         else:
                             LIST_ARR.append(arr_)
                             LIST_ARR_pred.append(predict)
-
-                    
-                        #print (arr_, arr_.shape)
-                        #LIST_ARR.append([[up1, P1],[dict_pid[Kx[0]], dict_category1[Kx[1]], dict_category2[Kx[2]], ]])
-                    #print ("\n")
+                        
     Array1 = np.array(LIST_ARR)
     Array2 = np.array(LIST_ARR_pred)
     print (Array1.shape, Array2.shape)
     Array1_test = np.array(LIST_ARR_test)
     Array2_test = np.array(LIST_ARR_pred_test)
+    print (Array1_test.shape, Array2_test.shape)
+    
+    OK = 0
+    NOK = 0
+    for k in range(Array2.shape[0]):
+        print (Array2[k,0])
+        if Array2[k,0] == 0:
+            NOK += 1
+        else:
+            OK += 1
+    print (OK, NOK)
     #---------------------------------->
     init = tf.initialize_all_variables()
     saver = tf.train.Saver()
@@ -334,28 +347,29 @@ def create_data():
         else:    
             sess.run(init)
         try:
-            for q in range(Array1.shape[0]):
-                batch_xs = Array1[q,:,:,:].reshape((1,6,3,1))
-                batch_ys = Array2[q,:].reshape((1,101))
-                #print (batch_xs.shape, batch_ys.shape)
-                sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-                
-                
-                
-                batch_test_x = Array1_test[10,:,:,:].reshape((1,6,3,1))
-                batch_test_y = Array2_test[10,:].reshape((1,101))                
-                r = sess.run([best,
-                              correct,
-                              tf.greater(y[:, 0], 0),
-                              y_[:, 0],
-                              loss],
-                              feed_dict={x: batch_test_x, y_: batch_test_y})
-                #r_short = (r[0][:190], r[1][:190])  
-                r_short = (r[0][:190], r[1][:190], r[2][:190], r[3][:190])            
-                for b, c, pb, pc in zip(*r_short):
-                    print ("{} {} <-> {} {}".format(c, pc, b, float(pb)))  
-                              
-                print ("Потеря: ", r[-1])
+            for a in range(20):
+                for q in range(Array1.shape[0]):
+                    batch_xs = Array1[q,:,:,:].reshape((1,6,3,1))
+                    batch_ys = Array2[q,:].reshape((1,101))
+                    #print (batch_xs.shape, batch_ys.shape)
+                    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+                    
+                    
+                    
+                    batch_test_x = Array1_test[-10:,:,:,:].reshape((10,6,3,1))
+                    batch_test_y = Array2_test[-10:,:].reshape((10,101))                
+                    r = sess.run([best,
+                                  correct,
+                                  tf.greater(y[:, 0], 0),
+                                  y_[:, 0],
+                                  loss],
+                                  feed_dict={x: batch_test_x, y_: batch_test_y})
+                    #r_short = (r[0][:190], r[1][:190])  
+                    r_short = (r[0][:190], r[1][:190], r[2][:190], r[3][:190])            
+                    for b, c, pb, pc in zip(*r_short):
+                        print ("{} {} <-> {} {}".format(c, pc, b, float(pb)))  
+                                  
+                    print ("Потеря: ", r[-1])
         except KeyboardInterrupt:
              save_path = saver.save(sess, "model_slim/model.ckpt")
              print("Model saved in path: %s" % save_path)
